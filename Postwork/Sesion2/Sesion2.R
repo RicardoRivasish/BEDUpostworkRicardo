d2dbbf5d06e187c3c506d3568f66b538396cad07
RicardoRivasish <47190559+RicardoRivasish@users.noreply.github.com>
2021-01-17 19:40:52 -0600

    Create Sesion2.R

---
+++ Postwork/Sesion2/Sesion2.R
@@ -0,0 +1,45 @@
+library(dplyr)
+#1.Importa los datos de soccer de las temporadas 2017/2018, 2018/2019 y 
+#2019/2020 de la primera división de la liga española a R, los datos los 
+#puedes encontrar en el siguiente enlace: https://www.football-data.co.uk/spainm.php
+#Se define el ambiente de trabajo
+getwd()
+setwd("C:/Users/Dell/Documents/GitHub/BEDUpostworkRicardo/Postwork/Sesion2/")
+#Se definen variables con urls
+B1920 <- "https://www.football-data.co.uk/mmz4281/1920/D1.csv"
+B1819 <- "https://www.football-data.co.uk/mmz4281/1819/D1.csv"
+B1718 <- "https://www.football-data.co.uk/mmz4281/1718/D1.csv"
+#Se descargan los documentos
+download.file(url = B1920, destfile = "B1920.csv", mode = "wb")
+download.file(url = B1819, destfile = "B1819.csv", mode = "wb")
+download.file(url = B1718, destfile = "B1718.csv", mode = "wb")
+#Se importan datos como una lista a R
+lista <- lapply(list.files(), read.csv)
+lista[[1]]
+
+
+#2.Obten una mejor idea de las características de los data frames
+#al usar las funciones: str, head, View y summary
+str(lista[[1]]); str(lista[[2]]); str(lista[[3]])
+head(lista[[1]]); head(lista[[2]]); head(lista[[3]])
+View(lista[[1]]); View(lista[[2]]); View(lista[[3]])
+summary(lista[[1]]); summary(lista[[2]]); summary(lista[[3]])
+
+#3.Con la función select del paquete dplyr selecciona únicamente 
+#las columnas Date, HomeTeam, AwayTeam, FTHG, FTAG y FTR; esto para
+#cada uno de los data frames. (Hint: también puedes usar lapply).
+lista<- lapply(lista, select, Date,HomeTeam,AwayTeam,FTHG,FTAG,FTR) 
+
+
+#4.Asegúrate de que los elementos de las columnas correspondientes 
+#de los nuevos data frames sean del mismo tipo (Hint 1: usa as.Date
+#y mutate para arreglar las fechas). Con ayuda de la función rbind 
+#forma un único data frame que contenga las seis columnas mencionadas 
+#en el punto 3 (Hint 2: la función do.call podría ser utilizada).
+str(lista[[1]]); str(lista[[2]]); str(lista[[3]])
+lista<-lapply(lista,mutate, Date=as.Date(Date, "%d/%m/%Y"))
+lista <- do.call(rbind, lista)
+head(lista)
+tail(lista)
+dim(lista)
+
